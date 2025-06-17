72946d5fe1fd9b9cf380399031938260017a1025
Salvo <salvatoresalanitro94@gmail.com>
2025-06-17 10:31:28 +0200

    add placelocation new model class and set to picked location in input location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:favorite_places_app/models/place.dart';
+import 'package:favorite_places_app/models/place_location.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
@@ -12,7 +14,7 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
   void _getCurrentLocation() async {
@@ -46,14 +48,24 @@ class _LocationInputState extends State<LocationInput> {
     final lat = locationData.latitude;
     final lng = locationData.longitude;
 
+    if (lat == null || lng == null) {
+      return;
+    }
+
     final url = Uri.parse(
       'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyDBPKOMXySrDxFRL5xMeWUuYdKQVIC1nnQ',
     );
+
     final response = await http.get(url);
     final responseData = json.decode(response.body);
     final address = responseData['results'][0]['formatted_address'];
 
     setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: lat,
+        longitude: lng,
+        address: address,
+      );
       _isGettingLocation = false;
     });
   }
