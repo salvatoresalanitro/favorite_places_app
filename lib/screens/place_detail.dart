32a7bb5c6685aa028dc4bf1b7f0f436a5c8cc08d
Salvo <salvatoresalanitro94@gmail.com>
2025-06-17 16:03:22 +0200

    add map of place in place detail

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -5,6 +5,13 @@ class PlaceDetail extends StatelessWidget {
   const PlaceDetail({super.key, required this.place});
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:A%7C$lat,$lng&key=AIzaSyDBPKOMXySrDxFRL5xMeWUuYdKQVIC1nnQ';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +32,43 @@ class PlaceDetail extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
-          )
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.transparent, Colors.black54],
+                      begin: Alignment.topCenter,
+                      end: Alignment.topCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    style: Theme.of(context).textTheme.titleLarge!
+                        .copyWith(
+                          color: Theme.of(
+                            context,
+                          ).colorScheme.onSurface,
+                        ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
